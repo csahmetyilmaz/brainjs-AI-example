2d03df8eb68da9170d9b4259eef2d4fbd371cc88
Ahmet YILMAZ <a.yilmaz@nicomatic.fr>
2022-02-09 19:44:31 +0100

    Added brain.js script

--- brain.js
+++ brain.js
@@ -1,17 +1,12 @@
-/*
- * Guide pour l'élaboration d'un script afin d'implémenter un algorithme de classification avec Brain.js 
- */
-
 const brain = require('brain.js');
 // => Cf. https://github.com/BrainJS/brain.js
 
-// Si CSV en local :
-// const readline = require('readline');
-// const fs = require('fs');
-// => Cf. https://nodejs.org/api/readline.html
+//TODO INFO:  J'ai passé par le CSV en ligne
+// TODO INFO: TP3 Ahmet YILMAZ &&  Jonathan Lopez GOMEZ
+//URL :   https://gist.githubusercontent.com/cbouvard/2f334a970cf543a507526bdca7d1cae4/raw/d535262ceea8fe71e4915b682ca01dc5e6d750f1/iris.csv
+ 
 
-// Si CSV via une URL :
-// const axios = require('axios');
+const axios = require('axios');
 // => Cf. https://github.com/axios/axios/
 
 /**
@@ -19,8 +14,11 @@ const brain = require('brain.js');
  * @param urlOrFilename
  * @returns csvData
  */
-function getCsvData(urlOrFilename) {
+async function getCsvData(urlOrFilename) {
     // TODO
+   // return fetch('data.json',)
+   const response = await axios.get(urlOrFilename,{responseType:'blob'});
+   return response.data;
 }
 
 /**
@@ -28,8 +26,26 @@ function getCsvData(urlOrFilename) {
  * @param csvData
  * @returns rawData
  */
-function parseCsv(csvData) {
-    // TODO
+async function parseCsv(csvData) {
+    let dataParsed = csvData.split('\n').slice(1);
+    let tab = [];
+    dataParsed.forEach(element => {
+        let objet=element.split(',');
+        switch (objet[4]){
+           case 'Setosa':
+               objet[4] = [1,0,0];
+               break;
+            case 'Versicolor':
+                objet[4] = [0,1,0];
+                break;
+            case 'Virginica':
+                objet[4] = [0,0,1];
+                break; 
+        }
+        tab.push({input: [parseFloat(objet[0]), parseFloat(objet[1]), parseFloat(objet[2]), parseFloat(objet[3])], output: objet[4]});
+    });
+    //log("tab", tab);
+    return tab;
 }
 
 /**
@@ -37,27 +53,61 @@ function parseCsv(csvData) {
  * @param rawData
  * @returns trainingData
  */
-function prepareTrainingData(rawData) {
-    // TODO
+async function prepareTrainingData(rawData) {
+    this.net = new brain.NeuralNetwork({
+        binaryThresh:0.5,
+        hiddenLayers:[3,3,2],
+        activation: "sigmoid"
+    });
+
+    this.net.train(rawData, {
+        iterations:1000,
+        learningRate:0.3
+    });
+
 }
 
+async function runningBrain(selectionnedData) {
+    return this.net.run(selectionnedData);
+}
 
 /**
  * Fonction principale du script
  */
-function main() {
+async function main() {
+    const url = "https://gist.githubusercontent.com/cbouvard/2f334a970cf543a507526bdca7d1cae4/raw/d535262ceea8fe71e4915b682ca01dc5e6d750f1/iris.csv";
+    
     // TODO: Appeler getCsvData
+    let csvData = await getCsvData(url);
 
     // TODO: Appeler parseCsv
+    let dataFormat = await parseCsv(csvData);
 
     // TODO: Appeler prepareTrainingData
-
     // TODO: Créer un NeuralNetwork de Brain.js
-
     // TODO: Entraîner le modèle (fonction train)
+    await prepareTrainingData(dataFormat);
+
 
     // TODO : Tester avec le lancement de prédiction (fonction run)
     // Astuce : la fonction run renvoie un tableau typé. Pour obtenir un tableau classique, utiliser la fonction Array.from
+   let datas = await runningBrain(dataFormat[60].input);
+    const resulTraining = Array.from(datas);
+    let setosa = resulTraining[0];
+    let versicolor = resulTraining[1];
+    let virginica = resulTraining[2];
+    let maxvalue = Math.max.apply(Math, resulTraining);
+    switch (maxvalue){
+        case setosa:
+           console.log("L'iris selectionné correspond à : "+ setosa +" de la fleur Setosa");
+            break;
+        case versicolor:
+            console.log("L'iris selectionné correspond à : "+ versicolor +" de la fleur Versicolor");
+            break;
+        case virginica:
+            console.log("L'iris selectionné correspond à : "+virginica+" de la fleur Virginica");
+            break;
+    }
 }
 
 main();
